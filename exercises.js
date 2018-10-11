02f24861807a1943e7c329fb405ee3460d4399be
kevin tauzer <kevintauzer97@gmail.com>
2018-10-11 11:30:47 -1000

    exercise 4, Blurple

--- exercises.js
+++ exercises.js
@@ -80,7 +80,11 @@ console.log(kicks);
 
   console.log the object
 */
-
+var plainBox = {};
+plainBox.color = "blurple"
+plainBox.size = 17
+plainBox.contents = [];
+console.log(plainBox);
 
 /*
 5. Declare a variable named `stockCar` and create the following properties (key-value pairs) to the object:
