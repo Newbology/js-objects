93c185fb9a4c337e6d190689584a00332c1da8a4
kevin tauzer <kevintauzer97@gmail.com>
2018-10-11 11:24:00 -1000

    exercise 3, being cool is easy

--- exercises.js
+++ exercises.js
@@ -61,8 +61,15 @@ console.log("My dog " + dog.name + " is " + dog.age +" years old and likes to " 
             
   Console.log the object.
 */
-
-
+var kicks = {};
+kicks.brand = "Nike, not NikE"
+kicks.color = "Colorful asf"
+kicks.size = 12
+kicks.price = 80
+kicks.buy = function(){
+    return "Now I i are coolest kid on block!"
+}
+console.log(kicks);
 /* 
 
 4. Declare a variable named `plainBox` and set its value to be an EMPTY object.
@@ -129,7 +136,59 @@ console.log("My dog " + dog.name + " is " + dog.age +" years old and likes to " 
             =====
             ...
  */
-
+var arrayOfObjects = [
+    {
+      id: 0,
+      date: "Monday Jan 25 2015 2:01 PM",
+      total: "279.38"
+    },
+    {
+      id: 1,
+      date: "Monday Jan 27 2015 11:31 AM",
+      total: "79.80"
+    },
+    {
+      id: 2,
+      date: "Monday Feb 1 2015 7:56 AM",
+      total: "15.62"
+    },
+    {
+      id: 3,
+      date: "Monday Feb 1 2015 9:43 AM",
+      total: "19.83"
+    },
+    {
+      id: 4,
+      date: "Monday Feb 1 2015 11:08 PM",
+      total: "56.69"
+    },
+    {
+      id: 5,
+      date: "Monday Feb 13 2015 10:22 AM",
+      total: "137.92"
+    },
+    {
+      id: 6,
+      date: "Monday Feb 14 2015 6:54 PM",
+      total: "938.65"
+    },
+    {
+      id: 7,
+      date: "Monday Feb 14 2015 7:17 PM",
+      total: "43.77"
+    },
+    {
+      id: 8,
+      date: "Monday Feb 14 2015 7:18 PM",
+      total: "28.54"
+    },
+    {
+      id: 9,
+      date: "Monday Feb 14 2015 7:18 PM",
+      total: "194.33"
+    }
+  ];
+  
 
 /*
 8. Addition with an object
