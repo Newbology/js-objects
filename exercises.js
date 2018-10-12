a3227b258bc0e3e57ea672353ffb4cf52fc8ba30
kevin tauzer <kevintauzer97@gmail.com>
2018-10-12 13:39:01 -1000

    exercise 9

--- exercises.js
+++ exercises.js
@@ -265,6 +265,12 @@ console.log(sumObjResult);
         **create more** objects and invoke your function multiple times.
  */
 
+function printObj(sum){
+  return sum.a + " + " + sum.b + " = " + sum.result;
+}
+console.log(printObj(sumObj));
+
+
 
 /*
 10. Putting stuff in `plainBox`
